b18c3ed48d04b3e7855b8c2414dbbb81c992dad2
earthern573 <197468245+earthern573@users.noreply.github.com>
2026-08-09 09:30:36 +0000

    feat: my exercise >  extract users by api

--- 00-bootcamp-project/main-api.py
+++ 00-bootcamp-project/main-api.py
@@ -30,6 +30,15 @@ data = "users"
 date = "2020-10-23"
 # ลองดึงข้อมูลจาก API เส้น users และเขียนลงไฟล์ CSV
 # YOUR CODE HERE
+response = requests.get(f"{API_URL}/{data}/?created_at={date}")
+data = response.json()
+with open(f"{DATA_FOLDER}/users.csv", "w") as f:
+    writer = csv.writer(f)
+    header = data[0].keys()
+    writer.writerow(header)
+
+    for each in data:
+        writer.writerow(each.values())
 
 ### Orders
 data = "orders"

61e3609fa5e85a19e1f5941781f89f84c1c4aac8
earthern573 <197468245+earthern573@users.noreply.github.com>
2026-08-09 09:21:36 +0000

    feat: my exercise >  extract order_items by direct database connection

--- 00-bootcamp-project/main-postgres.py
+++ 00-bootcamp-project/main-postgres.py
@@ -35,3 +35,13 @@ table = "order_items"
 header = ["order_id", "product_id", "quantity"]
 # ลองดึงข้อมูลจากตาราง order_items และเขียนลงไฟล์ CSV
 # YOUR CODE HERE
+with open(f"{DATA_FOLDER}/order_items.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+
+    query = f"select * from {table}"
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+    for each in results:
+        writer.writerow(each)

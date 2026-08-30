582d82718565bbdea5e2e83a7e97e952b6ee4730
earthern573 <197468245+earthern573@users.noreply.github.com>
2026-08-30 06:54:11 +0000

    [DEB-0604] hand-on > try template and operator

--- 04-automated-data-pipelines/dags/play_with_templating.py
+++ 04-automated-data-pipelines/dags/play_with_templating.py
@@ -9,6 +9,9 @@ def _get_date_part(ds, **context):
     print(ds)
     return ds_format(ds, "%Y-%m-%d", "%Y/%m/%d/")
 
+def _one_plus_one():
+    return 1+1
+
 
 with DAG(
     dag_id="play_with_templating",
@@ -27,6 +30,21 @@ with DAG(
     echo = BashOperator(
         task_id="echo",
         bash_command="echo {{ logical_date }}",
+    )
+
+    echo_1 = BashOperator(
+        task_id="echo_1",
+        bash_command="echo {{ data_interval_start }}",
+    )
+
+    echo_2 = BashOperator(
+        task_id="echo_2",
+        bash_command="echo {{ data_interval_end }}",
+    )
+
+    one_plus_one = PythonOperator(
+        task_id="one_plus_one",
+        python_callable=_one_plus_one,
     )
 
     # For this operator, name should be add as variable through Airflow UI

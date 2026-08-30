40c07a983ac00538ff80b4db3c0c2980123d0e9e
earthern573 <197468245+earthern573@users.noreply.github.com>
2026-08-30 04:16:05 +0000

    [DEB-0604] hand-on >> modify dependencies

--- 04-automated-data-pipelines/dags/dag_with_custom_task_dependencies.py
+++ 04-automated-data-pipelines/dags/dag_with_custom_task_dependencies.py
@@ -19,7 +19,17 @@ with DAG(
     t8 = EmptyOperator(task_id="t8")
     t9 = EmptyOperator(task_id="t9")
 
-    t1 >> t2 >> t3 >> t4 >> t9
-    t2 >> t6 >> t8 >> t9
-    t1 >> t5 >> t6
-    t5 >> t7 >> t8
+    # t1 >> t2 >> t3 >> t4 >> t9
+    # t2 >> t6 >> t8 >> t9
+    # t1 >> t5 >> t6
+    # t5 >> t7 >> t8
+
+    t1 >> [t2, t5]
+
+    t2 >> [t3, t6]
+    t5 >> [t6, t7]
+
+    t3 >> t4
+    [t6, t7] >> t8
+
+    [t4, t8] >> t9

6dc2ec6e476a899344f1f1db628aff20d26690b6
earthern573 <197468245+earthern573@users.noreply.github.com>
2026-09-06 09:49:07 +0000

    [DEB-0605] workshop-w05_hand-on add models to intermediate and marts

--- 00-bootcamp-project/dbt/greenery/models/marts/fct_orders.sql
+++ 00-bootcamp-project/dbt/greenery/models/marts/fct_orders.sql
@@ -1,40 +1,16 @@
 with
 
-orders as (
+int_orders_products__joined as (
 
-    select * from {{ ref('stg_greenery__orders') }}
-
-)
-
-, users as (
-
-    select * from {{ ref('stg_greenery__users') }}
-
-)
-
-, addresses as (
-
-    select * from {{ ref('stg_greenery__addresses') }}
-
-)
-
-, products as (
-
-    select * from {{ ref('stg_greenery__products') }}
-
-)
-
-, order_items as (
-
-    select * from {{ ref('stg_greenery__order_items') }}
+    select * from {{ ref('int_orders_products__joined') }}
 
 )
 
 , final as (
 
     select
-        o.order_guid
-        , o.order_created_at_utc
+        order_guid
+        , order_created_at_utc
         , order_cost_usd
         , shipping_cost_usd
         , order_total_usd
@@ -42,22 +18,18 @@ orders as (
         , estimated_delivery_at_utc
         , delivered_at_utc
         , order_status
-        , o.user_guid
-        , o.promo_guid
-        , u.created_at_utc as user_created_at_utc
-        , a.zipcode
-        , a.state
-        , a.country
-        , oi.quantity
+        , user_guid
+        , promo_guid
+        , user_created_at_utc
+        , zipcode
+        , state
+        , country
+        , quantity
         , product_name
         , price
         , inventory
 
-    from orders as o
-    left join users as u on o.user_guid = u.user_guid
-    left join addresses as a on o.address_guid = a.address_guid
-    left join order_items as oi on oi.order_guid = o.order_guid
-    left join products as p on oi.product_guid = p.product_guid
+    from int_orders_products__joined
 
 )
 

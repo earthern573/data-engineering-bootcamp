4811f7fba833fab0b045619c5e89b68411b38877
earthern573 <197468245+earthern573@users.noreply.github.com>
2026-09-20 04:47:23 +0000

    [DEB-0607] > 03-04 Update staging models, replace select *

--- 07-end-to-end-project/dbt/networkrail/models/staging/stg_networkrail__movements.sql
+++ 07-end-to-end-project/dbt/networkrail/models/staging/stg_networkrail__movements.sql
@@ -8,15 +8,50 @@ source as (
 
 , renamed_recasted as (
 
-    -- Your code here
-    select * from source
+    select
+        actual_timestamp
+        , auto_expected
+        , correction_ind
+        , current_train_id
+        , delay_monitoring_point
+        , direction_ind
+        , division_code
+        , event_source
+        , event_type
+        , gbtt_timestamp
+        , line_ind
+        , loc_stanox
+        , next_report_run_time
+        , next_report_stanox
+        , offroute_ind
+        , original_loc_stanox
+        , original_loc_timestamp
+        , planned_event_type
+        , planned_timestamp
+        , platform
+        , reporting_stanox
+        , route
+        , timetable_variation
+        , toc_id
+        , train_id
+        , train_file_address
+        , train_service_code
+        , train_terminated
+        , variation_status
+    from source
 
 )
 
 , final as (
 
-    -- Your code here
-    select * from renamed_recasted
+    select
+        event_type
+        , actual_timestamp
+        , event_source
+        , train_id
+        , variation_status
+        , toc_id
+    from renamed_recasted
 
 )
 

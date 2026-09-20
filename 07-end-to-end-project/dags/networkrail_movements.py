cd11f3e092044e0238e055439fe6688147f0acd2
earthern573 <197468245+earthern573@users.noreply.github.com>
2026-09-20 03:34:43 +0000

    [DEB-0607] > 02-02 Update spark tranformation function and load data to gcs function pipeline

--- 07-end-to-end-project/dags/networkrail_movements.py
+++ 07-end-to-end-project/dags/networkrail_movements.py
@@ -16,17 +16,52 @@ DAGS_FOLDER = "/opt/airflow/dags"
 BUSINESS_DOMAIN = "networkrail"
 DATA = "movements"
 LOCATION = "asia-southeast1"
-GCP_PROJECT_ID = "YOUR_GCP_PROJECT_ID"
-GCS_BUCKET = "YOUR_GCS_BUCKET"
+GCP_PROJECT_ID = "project-d069ecb2-d645-45e0-a1b"
+GCS_BUCKET = "deb-bootcamp-06-earth"
 BIGQUERY_DATASET = "networkrail"
-KEYFILE_FOR_GCS = "YOUR_KEYFILE_PATH"
-KEYFILE_FOR_GCS_TO_BIGQUERY = "YOUR_KEYFILE_PATH"
+KEYFILE_FOR_GCS = "/workspaces/data-engineering-bootcamp/00-bootcamp-project/deb-load-data-to-gcs.json"
+KEYFILE_FOR_GCS_TO_BIGQUERY = "/workspaces/data-engineering-bootcamp/00-bootcamp-project/deb-loading-to-bigquery.json"
 
 
 def _load_data_from_gcs_to_bigquery(data_interval_start, **context):
     ds = data_interval_start.to_date_string()
 
     # Your code here
+    keyfile_bigquery = KEYFILE_FOR_GCS_TO_BIGQUERY
+    service_account_info_bigquery = json.load(open(keyfile_bigquery))
+    credentials_bigquery = service_account.Credentials.from_service_account_info(
+        service_account_info_bigquery
+    )
+
+    bigquery_client = bigquery.Client(
+        project=GCP_PROJECT_ID,
+        credentials=credentials_bigquery,
+        location=LOCATION,
+    )
+
+    table_id = f"{GCP_PROJECT_ID}.{BIGQUERY_DATASET}.{DATA}"
+    job_config = bigquery.LoadJobConfig(
+        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+        source_format=bigquery.SourceFormat.PARQUET,
+        autodetect=True,
+        time_partitioning=bigquery.TimePartitioning(
+            type_=bigquery.TimePartitioningType.DAY,
+            field="actual_timestamp",
+    ),
+)
+
+    bucket_name = GCS_BUCKET
+    destination_blob_name = f"{BUSINESS_DOMAIN}/processed/*.parquet"
+    job = bigquery_client.load_table_from_uri(
+        f"gs://{bucket_name}/{destination_blob_name}",
+        table_id,
+        job_config=job_config,
+        location=LOCATION,
+    )
+    job.result()
+
+    table = bigquery_client.get_table(table_id)
+    print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
 
 
 default_args = {
@@ -36,7 +71,7 @@ default_args = {
 with DAG(
     dag_id="networkrail_movements",
     default_args=default_args,
-    schedule=None,  # Set the schedule here
+    schedule='@hourly',  # Set the schedule here
     catchup=False,
     tags=["DEB", "Skooldio", "networkrail"],
     max_active_runs=3,
@@ -46,10 +81,17 @@ with DAG(
     start = EmptyOperator(task_id="start")
 
     # Transform data in data lake using Spark
-    transform_data = EmptyOperator(task_id="transform_data")
+    transform_data = SparkSubmitOperator(
+        task_id="transform_data",
+        application="/opt/spark/pyspark/transform.py",
+        conn_id="my_spark",
+    )
 
     # Load data from GCS to BigQuery
-    load_data_from_gcs_to_bigquery = EmptyOperator(task_id="load_data_from_gcs_to_bigquery")
+    load_data_from_gcs_to_bigquery = PythonOperator(
+        task_id="load_data_from_gcs_to_bigquery",
+        python_callable=_load_data_from_gcs_to_bigquery,
+    )
 
     # End
     end = EmptyOperator(task_id="end", trigger_rule="one_success")
